c644976533056b1d5ff506de1ccc0e26b3baa6b3
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-07 11:32:40 +0200

    Array exercises

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+function unique(arr){
+    const uniques = [];
+    return arr.filter(item => {
+        if(uniques.includes(item)){
+            return false;
+        }
+        uniques.push(item);
+        return true;
+    })
+}
 
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
 
-
-
-
+alert( unique(strings) ); // Hare, Krishna, :-O
