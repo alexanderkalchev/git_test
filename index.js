30c4f2689cf218f6c12fdb05e2734757b526936b
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-08 12:51:37 +0200

    DOM exercise

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-function unique(arr){
-    const uniques = [];
-    return arr.filter(item => {
-        if(uniques.includes(item)){
-            return false;
-        }
-        uniques.push(item);
-        return true;
-    })
-}
+const container = document.querySelector("#container");
 
-let strings = ["Hare", "Krishna", "Hare", "Krishna",
-  "Krishna", "Krishna", "Hare", "Hare", ":-O"
-];
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
 
-alert( unique(strings) ); // Hare, Krishna, :-O
+const para = document.createElement("p");
+para.setAttribute("style", "color: red");
+para.innerText = "Hey I'm red";
+
+const h3 = document.createElement("h3");
+h3.setAttribute("style", "color: blue");
+h3.innerText = "I'm a blue h3!";
+
+const div = document.createElement("div");
+div.setAttribute("style", "border: 1px solid black, background-color: pink");
+const h1 = (document.createElement("h1"));
+const p = (document.createElement("p"));
+h1.innerText = "I’m in a div";
+p.innerText = "ME TOO"
+div.appendChild(h1);
+div.appendChild(p);
+
+container.appendChild(div);
+container.appendChild(h3);
+container.appendChild(para);
+container.appendChild(content);
